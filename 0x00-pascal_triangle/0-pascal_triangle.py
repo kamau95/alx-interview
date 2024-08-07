0aed32c99eb45bf68ab30542898660e9e4f456c5
kamau95 <linuskamau586@gmail.com>
2024-08-07 18:13:29 +0300

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    triangle = [[1]]  # Start with the first row
+    
+    for i in range(1, n):
+        row = [1]  # First element is always 1
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)  # Last element is always 1
+        triangle.append(row)
+    
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,1 @@
+Pascals triangle or Pascal's triangle is an arrangement of binomial coefficients in triangular form. It is named after the French mathematician Blaise Pascal.
